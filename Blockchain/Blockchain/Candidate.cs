70b0016e504749caf8fe14cdf43ab7411a97067d
arturtunevic <50708444+arturtunevic@users.noreply.github.com>
2019-05-22 20:31:49 +0300

    Add files via upload

---
+++ Blockchain/Blockchain/Candidate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChain
+{
+    public class Candidate
+    {
+        private string name;
+        private string img;
+        private int votes;
+
+        public Candidate(string name, string img)
+        {
+            this.name = name;
+            this.img = img;
+            votes = 0;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public string GetImg()
+        {
+            return img;
+        }
+
+        public int GetVotes()
+        {
+            return votes;
+        }
+
+        public void AddVote()
+        {
+            votes += 1;
+        }
+
+        public void SetVotesToZero()
+        {
+            votes = 0;
+        }
+    }
+}
---
+++ Blockchain/Blockchain/Dao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChain
+{
+    class DAO
+    {
+        List<Candidate> kandidatai;
+
+        public DAO()
+        {
+            kandidatai = new List<Candidate>();
+        }
+
+        public List<Candidate> CreateCandidates()
+        {
+            Candidate kandidatas1 = new Candidate("Ingrida Simonyte", "simonyte.jpg");
+            Candidate kandidatas2 = new Candidate("Gitanas Nauseda", "nauseda.jpg");
+            Candidate kandidatas3 = new Candidate("Saulius Skvernelis", "skvernelis.jpg");
+            kandidatai.Add(kandidatas1);
+            kandidatai.Add(kandidatas2);
+            kandidatai.Add(kandidatas3);
+            return kandidatai;
+        }
+    }
+}
